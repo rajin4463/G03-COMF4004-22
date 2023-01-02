4e258293287f2e567bd03c584491ebb3657048df
rajin4463 <rajinsandira@outlook.com>
2023-01-02 20:16:16 +0530

    added comments

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyPraser = require('body-parser');
 const connectDB = require('./dbconnect');
 const ShopDashRoutes = require('./routes/shopDashRoute');
-require('dotenv').config();
 const app = express();
 
 const PORT = 3000
 
+//use CORS 
 app.use(cors({
     origin: '*',
     methods: ['POST', 'GET', 'PATCH']
@@ -22,18 +23,21 @@ const start = async () =>{
     }
 };
 
+//Connect to mongoDB
 start();
 
 console.log('Server starting.....');
+//listen on PORT
+app.listen(PORT)
 
+//body-praser used for post requests
 app.use(bodyPraser.urlencoded({extended: false}));
 app.use(bodyPraser.json());
 
 //Routes
 app.use('/shopdash', ShopDashRoutes);
 
-app.listen(PORT)
-
+//default landing page
 app.get('/', (request, response) => {
     response.send('<body style = "background-color: rgba(0, 0, 0, 0.82); color:antiquewhite; padding:2%;"><h1> MALL51 API endpoint</h1></body>')
 });
