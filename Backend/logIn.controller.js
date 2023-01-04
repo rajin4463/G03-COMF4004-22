bbc0efad5cca6c9dc983fe02e3123a85a7142ee5
SanduniSrikantha <CB010303@students.apiit.lk>
2023-01-04 17:15:09 +0530

    login controller functions - edited

--- Backend/logIn.controller.js
+++ Backend/logIn.controller.js
@@ -1,58 +1,7 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true)
-const bodyPraser = require('body-parser');
+require('dotenv').config();
+const Credentials = require('../models/credentials');
 
-app.use(bodyPraser.urlencoded({extended: false}));
-app.use(bodyPraser.json());
-
-const path = require('path');
-const Credentials = require('./models/credentials');
-require ('dotenv').config({ path: path.resolve(__dirname, './.env')});
-
-const dburi = process.env.URI
-
-mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true})
- .then((result)=>{
-    console.log('connected to db...');
- }).catch((err)=>{
-    console.log(err);
-})
-
-
-
-//SHOP MANAGER LOGIN 
-async function ShopManLogIn(req, res){
-    try {
-        const {UserName, Password} = req.body;
-
-        if (!(UserName && Password)){
-            res.status(400).send("All input required");
-        }
-
-        const shopman_user = await Credentials.findOne({UserName:UserName});
-
-        if (shopman_user && (await compare(Password, shopman_user.Password))) {
-            
-        const AD_searchID = ({ShopID: shopman_user.ShopID});
-        res.send({ShopID: shopman_user.ShopID});
-        if (AD_searchID == ADMINID){
-            //local storage 
-            localStorage.setItem("SHOPID", "9978")
-            localStorage.setItem("Role", "Admin")
-        }
-        }else{
-            return res.status(400).send("Invalid credentials");
-        }   
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-
-//ADMIN LOGIN
+//ADMIN LOGIN 
 async function AdminLogIn(req, res){
     try {
         const {UserName, Password} = req.body;
@@ -63,17 +12,22 @@ async function AdminLogIn(req, res){
 
         const admin_user = await Credentials.findOne({UserName:UserName});
 
-        if (admin_user && (await compare(Password, admin_user.Password))) {
+        const admin_password = admin_user.Password;
+        //if (admin_user && (await compare(Password, admin_user.Password)))
+        if(admin_user && (Password == admin_password ))
+        //if (admin_user && (await compare(Password,admin_password)))
+    {
             
-        const SM_searchID = ({ShopID: shopman_user.ShopID});
-        res.send({ShopID: shopman_user.ShopID});
-        console.log(SM_searchID)
-
-        //local storage 
-        localStorage.setItem("ShopID", SM_searchID)
-        localStorage.setItem("Role", "ShopMan")
-            
-
+        const AD_searchID = admin_user.ShopID;
+        // res.send({ShopID: admin_user.ShopID});
+        if (AD_searchID == process.env.ADMINID){
+            //local storage 
+            res.send({
+                "ShopID": AD_searchID,
+                "Role":"Admin"
+            })
+    
+        }
         }else{
             return res.status(400).send("Invalid credentials");
         }   
@@ -84,9 +38,38 @@ async function AdminLogIn(req, res){
 }
 
 
+//SHOP MANAGER LOGIN
+async function ShopManLogIn(req, res){
+    try {
+        const {UserName, Password} = req.body;
 
-//TRIGGER BUTTONS 
-//adminLogIn shopManLogIn 
+        if (!(UserName && Password)){
+            res.status(400).send("All input required");
+        }
 
-document.getElementById("adminLogIn").onsubmit = async function() {AdminLogIn()};
-document.getElementById("shopManLogIn").onsubmit = async function() {ShopManLogIn()};
+        const shopman_user = await Credentials.findOne({UserName:UserName});
+
+        const shopman_password = shopman_user.Password;
+
+        if(shopman_user && (Password == shopman_password )) {
+            
+        const SM_searchID = shopman_user.ShopID;
+        //res.send({ShopID: shopman_user.ShopID});
+        //console.log(SM_searchID)
+
+        //local storage 
+        res.send({
+            "ShopID":SM_searchID,
+            "Role":"Shop Manager"
+        })
+            
+
+        }else{
+            return res.status(400).send("Invalid credentials");
+        }   
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+module.exports = {AdminLogIn, ShopManLogIn}
