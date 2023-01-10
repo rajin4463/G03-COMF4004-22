dcf46a414d79f8966e447b778c8f9172641a8ce6
Rajin|APIIT <cb009455@students.apiit.lk>
2023-01-10 12:22:35 +0530

    controller chnages

--- Backend/controllers/homeController.js
+++ Backend/controllers/homeController.js
@@ -1,18 +1,6 @@
 const ShopDetails = require('../models/Shopdetails');
 const ShopImage = require('../models/shopImg');
 
-exports.shop_details_get = (req, res) => {
-    // res.send('<body style = "background-color: rgba(0, 0, 0, 0.82); color:antiquewhite; padding:2%;"><h1> MALL51 Home Page</h1></body>');
-    ShopDetails.find({}, function(error, details){
-        if(error){
-            console.log(error);
-        }
-        else{
-            res.send({shopDetails: details});
-        }
-    })
-}
-
 exports.shop_img_get = (req, res) => {
     // res.send('<body style = "background-color: rgba(0, 0, 0, 0.82); color:antiquewhite; padding:2%;"><h1> MALL51 Home Page</h1></body>');
     try{
@@ -30,28 +18,42 @@ exports.shop_img_get = (req, res) => {
     }
 }
 
-// Search Function
+// Function - Get shop details
 
-exports.shop_search_get = (req, res) => {
-    const {shopName} = req.params;
-    ShopDetails.findOne({shopname: shopName}, function(error, result){
+exports.shop_details_get = (req, res) => {
+    let ShopDetailsArray = [];
+     //res.send('<body style = "background-color: rgba(0, 0, 0, 0.82); color:antiquewhite; padding:2%;"><h1> MALL51 Home Page</h1></body>');
+    ShopDetails.find({}, function(error, details){
         if(error){
             console.log(error);
         }
         else{
-            res.status(200).json({ShopName: result.ShopName, ShopLocation: result.Location});
+            for(let i = 0; i < details.length; i++){
+                ShopDetailsArray.push({ShopName: details[i].shopname, ShopLocation: details[i].location, Discount: details[i].discounts})
+            }
+            res.json(ShopDetailsArray);
+            console.log(ShopDetailsArray);
+            
         }
     })
 }
 
-exports.shop_category_search = (req, res) => {
-    const {category} = req.params;
-    ShopDetails.findOne({Categories: category}, function(error, result){
+// Search Function
+
+exports.shop_search_get = (req, res) => {
+    const searchType = req.params[0];
+    const searchValue = req.query[searchType];
+    let Shops = [];
+    ShopDetails.find({$or:[{shopname: searchValue},{categories: searchValue}]}, function(error, result){
         if(error){
             console.log(error);
         }
         else{
-            res.json({ShopName: result.ShopName, ShopLocation: result.Location})
+            for(let i = 0; i < result.length; i++){
+                Shops.push({ShopName: result[i].shopname, ShopLocation: result[i].location, Discount: result[i].discounts});  
+            }
+            res.json(Shops);
+            console.log(Shops);
         }
     })
 }
