24c765c2e4223b7f0b699f7e0209463c0f0a35e0
anngithub1234 <cb009582@students.apiit.lk>
2023-01-20 01:09:37 +0530

    added comments

--- Backend/controllers/admincontroller.js
+++ Backend/controllers/admincontroller.js
@@ -1,14 +1,14 @@
-const cred = require('../models/credentials');
+const cred = require('../models/credentials');  // required models
 const User = require('../models/userDetails');
 const ShopDe = require('../models/Shopdetails');
 
 //user details & user credentials store function
-async function usrDetails(req, res){
-    const {ShopID, UserName, Password, FirstName, LastName} = req.body
+async function usrDetails(req, res){   // takes both request and response
+    const {ShopID, UserName, Password, FirstName, LastName} = req.body      //destructures the request body to extract the values
     try{
-        const newCred = await cred.create({ShopID, UserName, Password})
+        const newCred = await cred.create({ShopID, UserName, Password})   // create a new document with the extracted values
         const newUser = await User.create({ShopID, FirstName, LastName})
-        res.send({status: "Success!"})
+        res.send({status: "Success!"})   // checks status of the created document
     }
     catch(err){
         console.log(err);
@@ -17,12 +17,12 @@ async function usrDetails(req, res){
 }
 
 //shop details store function
-async function shopDetails(req, res){
-    const {ShopID, ShopName, Location, Discounts} = req.body;
+async function shopDetails(req, res){   // takes both request and response
+    const {ShopID, ShopName, Location, Discounts} = req.body;      //destructures the request body to extract the values
     const {Category} = req.body;
     try{
-        const Shop = await ShopDe.create({ShopID, ShopName, Location, Category, Discounts})
-        res.send({status: "Success!"})
+        const Shop = await ShopDe.create({ShopID, ShopName, Location, Category, Discounts})   // create a new document with the extracted values
+        res.send({status: "Success!"})   // checks status 
     }
     catch(err){
         console.log(err);
@@ -31,4 +31,4 @@ async function shopDetails(req, res){
 
 }
 
-module.exports = {usrDetails, shopDetails};
+module.exports = {usrDetails, shopDetails};  // eports both the functions 
